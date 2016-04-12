18dcd80becbd3bf5221f468d90a9f82509d14657
fozzz <fozzmarc@gmail.com>
2016-04-12 16:26:28 +0800

    Update apiutils.py
    添加加密方式

--- toughlib/apiutils.py
+++ toughlib/apiutils.py
@@ -18,9 +18,21 @@ def make_sign(api_secret, params=[]):
     mds = md5(strs.encode('utf-8')).hexdigest()
     return mds.upper()
 
+def c_make_sign(params={}):
+    """
+        >>> c_make_sign({"k1":"v1", "k2":"v2"})
+        '33C9065427EECA3490C5642C99165145'
+    """
+    _params = [utils.safeunicode(k) for k in params if k is not None]
+    _params.sort()
+    _params2 = [utils.safeunicode(k)+'='+params[k]+'&' for k in _params if k is not None]
+    strs = ''.join(_params2).strip('&')
+    mds = md5(strs.encode('utf-8')).hexdigest()
+    return mds.lower()
 
 def check_sign(api_secret, msg):
     """
+        自定义加密串
         >>> check_sign("123456",dict(code=1,s='2',msg=u'中文',sign='33C9065427EECA3490C5642C99165145'))
         True
 
@@ -30,6 +42,18 @@ def check_sign(api_secret, msg):
     sign = msg['sign']
     params = [utils.safestr(msg[k]) for k in msg if k != 'sign']
     local_sign = make_sign(api_secret, params)
+    return sign == local_sign
+
+def c_check_sign(msg):
+    """
+        自定义加密
+        >>> c_check_sign(dict(code=1,s='2',msg=u'中文',sign='33C9065427EECA3490C5642C99165145'))
+        True
+    """
+    if "sign" not in msg:
+        return False
+    sign = msg.pop('sign')
+    local_sign = c_make_sign(msg)
     return sign == local_sign
 
 def make_message(api_secret, enc_func=False, **params):
@@ -65,6 +89,25 @@ def parse_request(api_secret, reqbody, dec_func=False):
     if not check_sign(api_secret, req_msg):
         raise ValueError(u"message sign error")
 
+    return req_msg
+
+def c_parse_request(reqbody, dec_func=False):
+    """
+        没有token进行加密 - 适用于卡平台的特殊加密
+        >>> c_parse_request('{"nonce": 1451122677, "msg": "helllo", "code": 0, "sign": "DB30F4D1112C20DFA736F65458F89C64"}')
+        {u'nonce': 1451122677, u'msg': u'helllo', u'code': 0, u'sign': u'DB30F4D1112C20DFA736F65458F89C64'}
+    """
+    try:
+        if callable(dec_func):
+            req_msg = json.loads(dec_func(reqbody))
+        else:
+            req_msg = json.loads(reqbody)
+    except Exception as err:
+        raise ValueError(u"parse params error")
+    
+    if not c_check_sign(req_msg):
+        raise ValueError(u"message sign error")
+        
     return req_msg
 
 def request(apiurl, data=None, **kwargs):
